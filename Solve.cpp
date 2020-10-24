077a636bba1f22fbb57fdb854b20e55673a25a8b
hu.fwh <hu.fwh@alibaba-inc.com>
2020-10-25 02:14:36 +0800

    add C++ contest code template

---
+++ Solve.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <algorithm>
+#include <stack>
+#include <bitset>
+#include <string>
+#include <cmath>
+#include <list>
+#include <cstring>
+#include <queue>
+
+using namespace std;
+
+typedef long long int LL;
+typedef unsigned long long int ULL;
+
+#define PI acos(-1.0)
+#define MOD 1e9+7
+#define INF 1e9
+#define PII pair<int,int>
+#define PL cout<<"-----"<<endl;
+#define MP make_pair
+#define PB push_back
+#define fst first
+#define snd second
+#define QUICKEN ios::sync_with_stdio(false),cin.tie(0),cout.tie(0);
+#define PR(x) cout<<x<<endl;
+
+int main() {
+  return 0;
+}
